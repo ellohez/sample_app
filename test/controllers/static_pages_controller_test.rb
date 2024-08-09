5441929f17597905c27e0d4522be287ae1a834df
Helen Pickavance <Helen.Pickavance@digital.cabinet-office.gov.uk>
2024-08-09 14:36:15 +0100

    Added db test files to ignore. Added comments to explain auto generated tests. Added minitest reporters to test helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,8 +1,14 @@
 require "test_helper"
 
+# The auto created version of a test will just add a simple test for each action
+# in the controller.
+# Each test simply 'gets' a URL and uses an assertion
+# to verify that the result is a success.
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
+    # The :success response is and abstract representation of the
+    # HTTP status code - 200 OK
     assert_response :success
   end
 
