76edf92a62416441ad913d79908cacf1ca746a56
ellohez <helen.pickavance@digital.cabinet-office.gov.uk>
2024-09-06 17:03:30 +0100

    Use rails routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,35 +10,29 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
-  test "should get the root" do
-    get root_url
+  test "should get home (root)" do
+    get root_path
 
-    assert_response :success
-    assert_select "title", "#{@base_title}"
-  end
-
-  test "should get home" do
-    get static_pages_home_url
-    # The :success response is and abstract representation of the
+    # The :success response is an abstract representation of the
     # HTTP status code - 200 OK
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
     assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_path
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
