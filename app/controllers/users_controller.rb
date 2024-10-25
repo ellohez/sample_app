e2dd3f8dac25c425aa15f99b5f7e4a1ee1739097
ellohez <helen.pickavance@digital.cabinet-office.gov.uk>
2024-10-25 11:01:59 +0100

    Add user sign up form
    - user controller
    - helper for gravatar (profile pic)
    - show view for user
    - css and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
-# Class description goes here
+# UsersController - description needed
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
 end
