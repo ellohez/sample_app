9632a36443b0387caa734e2c6cbc6787ec6ef7ba
ellohez <helen.pickavance@digital.cabinet-office.gov.uk>
2024-11-01 16:32:52 +0000

    Chapter 7.4 successful signups
    - allow user to create a new account and be directed to new profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params) # Not final implementation!
+    if @user.save
+      flash[:success] = "Welcome to the Sample App #{@user.name}!"
+      # Rails auto infers that we want to redirect_to user_url(@user)
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
