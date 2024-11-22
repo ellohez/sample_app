d3502eb51d83124e00f4314f28df8cfbd1d0fed0
ellohez <ellohez@gmail.com>
2024-11-22 16:57:25 +0000

    Chapter 9 - Advanced login
    - Implement remember me checkbox
    - Include token and digest for above
    - Testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,13 +14,33 @@ module ActiveSupport
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
+    # To allow us to test for the right title
+    include ApplicationHelper
+
+    # Add more helper methods to be used by all tests here...
+
     # Returns true if a test user is logged in.
     def is_logged_in?
       !session[:user_id].nil?
     end
 
-    # Add more helper methods to be used by all tests here...
-    # To allow us to test for the right title
-    include ApplicationHelper
+    #  Inside controller tests, we can manipulate the session method directly,
+    # assigning user.id to the :user_id key
+    def log_in_as(user)
+      session[:user_id] = user.id
+    end
+  end
+end
+
+module ActionDispatch
+  class IntegrationTest
+    # Because it’s located inside the ActionDispatch::IntegrationTest class,
+    # this is the version of log_in_as that will be called inside integration tests
+    # Inside integration tests, we can’t manipulate session directly, but we can post to the sessions path
+    def log_in_as(user, password: 'password', remember_me: '1')
+      post login_path, params: { session: { email: user.email,
+                                            password: password,
+                                            remember_me: remember_me} }
+    end
   end
 end
