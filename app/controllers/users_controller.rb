7efef22f5cd68987afe96728b047d3c2b811c4b8
ellohez <ellohez@gmail.com>
2024-12-06 17:56:21 +0000

    Section 10.2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,8 @@
 
 # UsersController - description needed
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -42,5 +44,21 @@ class UsersController < ApplicationController
   # Strong parameters used to prevent mass assignment vulnerability
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  # Before filters
+  # Confirms a logged-in user
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'Please log in'
+      redirect_to login_url, status: :see_other
+    end
+  end
+
+  # Confirms the correct user
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless @user == current_user
   end
 end
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -8,6 +8,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test 'unsuccessful edit' do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: '',
@@ -18,7 +19,13 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert', 'The form contains 4 errors'
   end
 
-  test 'successful edit' do
+  test 'successful edit with friendly forwarding' do
+    get edit_user_path(@user)
+    assert_equal edit_user_url(@user), session[:forwarding_url]
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert session[:forwarding_url].nil?
+
     # Submit valid user information
     get edit_user_path(@user)
     name = 'Ford Prefect'
