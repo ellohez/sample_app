c6a303bb3d1f3774ea28f2a61b3c46c664636065
ellohez <ellohez@gmail.com>
2025-09-19 17:26:15 +0100

    Re-factor test to use helper method

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,34 +7,30 @@ require 'test_helper'
 # Each test simply 'gets' a URL and uses an assertion
 # to verify that the result is a success.
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @base_title = 'Ruby on Rails Tutorial Sample App'
-  end
-
   test 'should get home (root)' do
     get root_path
 
     # The :success response is an abstract representation of the
     # HTTP status code - 200 OK
     assert_response :success
-    assert_select 'title', @base_title.to_s
+    assert_select 'title', full_title
   end
 
   test 'should get help' do
     get help_path
     assert_response :success
-    assert_select 'title', "Help | #{@base_title}"
+    assert_select 'title', full_title('Help')
   end
 
   test 'should get about' do
     get about_path
     assert_response :success
-    assert_select 'title', "About | #{@base_title}"
+    assert_select 'title', full_title('About')
   end
 
   test 'should get contact' do
     get contact_path
     assert_response :success
-    assert_select 'title', "Contact | #{@base_title}"
+    assert_select 'title', full_title('Contact')
   end
 end
