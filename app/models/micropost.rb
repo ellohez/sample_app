d536a145bb4a25c2cea433eb824886a6ae1b6e02
ellohez <ellohez@gmail.com>
2025-10-17 16:45:39 +0100

    Add image upload for microposts

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   # The default_scope method allows us to arrange for a default ordering on the results of a database query
   # In this case, we arrange for microposts to be retrieved in descending order of creation
   # This uses the 'stabby lamda' syntax -> { ... } to create an anonymous function
